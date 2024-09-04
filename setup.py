e93b27fbe5c04d913584ef8eb555e1d5a23c6e81
Anto Idicherian Lonappan <antolonappan@icloud.com>
2024-09-03 17:11:49 -0700

    conda
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+    
+setup(
+    name="echolens",
+    version="0.1",
+    packages=find_packages(include=['echolens', 'echolens.*']),
+    include_package_data=True,
+    description="A package for CMB simulations and instrument modeling",
+    maintainer="Anto Idicherian Lonappan",
+    maintainer_email="your_email@example.com",  # Replace with your actual email
+    package_data={
+        'echolens.simulation': ['echo.ini', 'masks.fits', 'spectra.pkl'],  # Include your data files
+    },
+)
