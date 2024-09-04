cebad264ad2225fd7a3de5b94d59e3a5f9983e90
Anto Idicherian Lonappan <antolonappan@icloud.com>
2024-09-03 18:17:22 -0700

    updated the job.py
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

--- slurm/jobs.py
+++ slurm/jobs.py
@@ -1,13 +1,28 @@
 import argparse
+import toml
 parser = argparse.ArgumentParser(description='ini')
 parser.add_argument('config', type=str, nargs=1)
-parser.add_argument('-sky', dest='Simulation pipeline to generate HILC component seperated CMB maps', action='store_true', help='map making')
-parser.add_argument('-n', type=int, help='number of simulations')
+parser.add_argument('-s','--sky', dest='sky', action='store_true', help='Simulation pipeline to generate HILC component seperated CMB maps')
+parser.add_argument('-f','--filter', dest='filter', action='store_true', help='C inverse filtering pipeline for CS maps')
+parser.add_argument('-r','--recon', dest='recon', action='store_true', help='Reconstruction pipeline for CS maps')
+parser.add_argument('-n','--nsims', type=int, help='number of simulations')
 args = parser.parse_args()
 
-ini = args.inifile[0]
+ini = args.config[0]
+
+if args.nsims is None:
+    try:
+        nsims = toml.load(ini)['simulation']['nsims']
+    except KeyError:
+        raise ValueError('nsims not found in ini file or not provided as an argument, please provide nsims')
 
 if args.sky:
     from echolens import simulation
     sim = simulation.CMBbharatSky.from_file(ini)
-    sim.make_sims(args.n)
+    sim.make_sims(args.nsims)
+
+if args.filter:
+    raise NotImplementedError
+
+if args.recon:
+    raise NotImplementedError
